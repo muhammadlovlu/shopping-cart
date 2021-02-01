16256d95c805cacde28b6cd62a5deb3d13d9d779
muhammadlovlu <chyshahab@gmail.com>
2021-02-01 13:45:22 +0600

    updated

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+
+document.getElementById("increase").addEventListener('click', function(){
+    incNumber('qtyUpdate1');    
+
+document.getElementById("decrease").addEventListener('click', function(){
+    decNumber('qtyUpdate1');
+})
+
+document.getElementById("incItem2").addEventListener('click', function(){
+    incNumber('qtyUpdate2');    
+})
+
+document.getElementById("deItem2").addEventListener('click', function(){
+    decNumber('qtyUpdate2');
+})
+})
+function incNumber(id){
+    var i = parseInt(document.getElementsByClassName(id)[0].value);
+    i++;
+    document.getElementsByClassName(id)[0].value = i;
+}
+function decNumber(id){
+    var i = parseInt(document.getElementsByClassName(id)[0].value);
+    i--;
+    document.getElementsByClassName(id)[0].value = i;
+}
