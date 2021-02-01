8acaa84f015f880f939b8cfa7d5c40e46db9fba8
muhammadlovlu <chyshahab@gmail.com>
2021-02-01 13:48:54 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,10 @@
 
 document.getElementById("increase").addEventListener('click', function(){
     incNumber('qtyUpdate1');    
-
+})
 document.getElementById("decrease").addEventListener('click', function(){
     decNumber('qtyUpdate1');
 })
-
 document.getElementById("incItem2").addEventListener('click', function(){
     incNumber('qtyUpdate2');    
 })
@@ -13,7 +12,8 @@ document.getElementById("incItem2").addEventListener('click', function(){
 document.getElementById("deItem2").addEventListener('click', function(){
     decNumber('qtyUpdate2');
 })
-})
+
+
 function incNumber(id){
     var i = parseInt(document.getElementsByClassName(id)[0].value);
     i++;
