8034706e650d4ffef15a447af72baaf0b2341675
muhammadlovlu <chyshahab@gmail.com>
2021-02-02 10:39:39 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,42 @@
-
-document.getElementById("increase").addEventListener('click', function(){
-    incNumber('qtyUpdate1');    
-})
-document.getElementById("decrease").addEventListener('click', function(){
-    decNumber('qtyUpdate1');
-})
-document.getElementById("incItem2").addEventListener('click', function(){
-    incNumber('qtyUpdate2');    
-})
-
-document.getElementById("deItem2").addEventListener('click', function(){
-    decNumber('qtyUpdate2');
+document.getElementById('increase').addEventListener('click', function (){
+    document.getElementById('')
 })
 
 
-function incNumber(id){
-    var i = parseInt(document.getElementsByClassName(id)[0].value);
-    i++;
-    document.getElementsByClassName(id)[0].value = i;
-}
-function decNumber(id){
-    var i = parseInt(document.getElementsByClassName(id)[0].value);
-    i--;
-    document.getElementsByClassName(id)[0].value = i;
-}
+
+
+
+
+
+
+// I tried myself, to figure out how it can works 
+
+
+
+// document.getElementById("increase").addEventListener('click', function(){
+//     incNumber('qtyUpdate1');    
+// })
+// document.getElementById("decrease").addEventListener('click', function(){
+//     decNumber('qtyUpdate1');
+// })
+// document.getElementById("incItem2").addEventListener('click', function(){
+//     incNumber('qtyUpdate2');
+// })
+
+// document.getElementById("deItem2").addEventListener('click', function(){
+//     decNumber('qtyUpdate2');
+// })
+
+
+// function incNumber(id){
+//     var i = parseInt(document.getElementsByClassName(id)[0].value);
+//     i++;
+//     document.getElementsByClassName(id)[0].value = i;
+// }
+
+
+// function decNumber(id){
+//     var i = parseInt(document.getElementsByClassName(id)[0].value);
+//     i--;
+//     document.getElementsByClassName(id)[0].value = i;
+// }
